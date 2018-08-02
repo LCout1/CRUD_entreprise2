0c1ff267f000c62c19c203289b245ead5604ac13
Louis COUTIN <louiscoutin@gmail.com>
2018-08-02 16:21:49 +0200

    correction

--- include/init.php
+++ include/init.php
@@ -1,6 +1,6 @@
 <?php 
 // ----- connection bdd
-$pdo = new PDO('mysql:host=localhost;dbname=entreprise', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT => 'SET NAMES utf8'));
+$pdo = new PDO('mysql:host=localhost;dbname=entreprise', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
 session_start();
 
